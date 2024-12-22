3170e1ebb1427815eeab92c117ab0af0b6367f1d
Arif Rachman <emailearif@gmail.com>
2024-12-22 07:04:14 +0700

    update dashboard eveloper

--- app/Views/developer/v_dashboard.php
+++ app/Views/developer/v_dashboard.php
@@ -55,8 +55,6 @@
           <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
-    </div>
-    <div class="row">
       <div class="col-lg-3 col-6">
         <!-- small box -->
         <div class="small-box bg-info">
@@ -66,48 +64,6 @@
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-success">
-          <div class="inner">
-            <h3 class="text-center"><?= $reportunit->validsikumbang; ?></h3>
-            <p>Unit Tervalidasi Sikumbang</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-stats-bars"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-warning">
-          <div class="inner">
-            <h3 class="text-center"><?= $reportunit->valideflpp; ?></h3>
-            <p>Unit Tervalidasi FLPP</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-person-add"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h3 class="text-center"><?= $reportunit->validsp3k; ?></h3>
-            <p>Unit Tervalidasi SP3K</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-pie-graph"></i>
           </div>
           <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
